0e8ab831e0a0c950ce71a27ad04fa520906ebe5d
liuyujieccnu <liuyujie@mails.ccnu.edu.cn>
2019-09-18 14:09:00 +0800

    first edition finished

---
+++ spring-boot-chatroom/src/main/java/edu/udacity/java/nano/chat/Message.java
@@ -0,0 +1,55 @@
+package edu.udacity.java.nano.chat;
+
+/**
+ * WebSocket message model
+ */
+public class Message {
+    // TODO: add message model.
+    private String content;
+    private String username;
+    private int onlineCount;
+    private String type;
+
+    public Message() {
+    }
+
+    public Message(String username,String content,int onlineCount,String type) {
+        this.username = username;
+        this.content = content;
+        this.onlineCount = onlineCount;
+        this.type=type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getOnlineCount() {
+        return onlineCount;
+    }
+
+    public void setOnlineCount(int onlineCount) {
+        this.onlineCount = onlineCount;
+    }
+
+}
---
+++ spring-boot-chatroom/src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
@@ -0,0 +1,83 @@
+package edu.udacity.java.nano.chat;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * WebSocket Server
+ *
+ * @see ServerEndpoint WebSocket Client
+ * @see Session   WebSocket Session
+ */
+
+@Component
+//该注解用来指定一个URI，客户端可以通过这个URI来连接到WebSocket。类似Servlet的注解mapping。
+@ServerEndpoint("/chat/{username}")//可以向服务器端传递参数
+public class WebSocketChatServer {
+
+    /**
+     * All chat sessions.
+     */
+    // 保存所有客户端的链接会话
+    private static Map<String, Session> onlineSessions = new ConcurrentHashMap<>();
+    private static HashMap<String, String> users = new HashMap<>();
+
+    private static void sendMessageToAll(String msg) {
+        //TODO: add send message method.
+        onlineSessions.forEach((id,session)->{
+                session.getAsyncRemote().sendText(msg);//循环在线的onlineSessions的session，向每个用户发消息
+        });
+
+    }
+
+    /**
+     * Open connection, 1) add session, 2) add user.
+     */
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String user) {
+        //TODO: add on open connection.
+        onlineSessions.put(session.getId(), session); //将本次链接会话以"ID-session"的形式保存
+        users.put(session.getId(),user);
+        Message message = new Message(user,"ENTERED THE CHAT",onlineSessions.size(),"ENTER");
+        sendMessageToAll(JSON.toJSONString(message));
+    }
+
+    /**
+     * Send message, 1) get username and session, 2) send message to all.
+     */
+    @OnMessage
+    public void onMessage(Session session, String jsonStr) {
+        //TODO: add send message.
+        Message message = JSON.parseObject(jsonStr,Message.class);//把客户端传来的Json格式的数据转换成message对象
+        Message messageSend = new Message(message.getUsername(),message.getContent(),onlineSessions.size(),"SPEAK");
+        sendMessageToAll(JSON.toJSONString(messageSend));
+    }
+
+    /**
+     * Close connection, 1) remove session, 2) update user.
+     */
+    @OnClose
+    public void onClose(Session session) {
+        //TODO: add close connection.
+        onlineSessions.remove(session.getId());
+        Message message=new Message(users.get(session.getId()),"LEAVE THE CHAT",onlineSessions.size(),"LEAVE");
+        users.remove(session.getId());
+        sendMessageToAll(JSON.toJSONString(message));
+    }
+
+    /**
+     * Print exception.
+     */
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+
+}
